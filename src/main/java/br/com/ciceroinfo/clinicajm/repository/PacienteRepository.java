3b025adc5408fa6eeba99f1d89ae3362605287df
P9912577 <P9912577@CTARO11>
2015-07-15 16:09:54 -0300

    .

---
+++ src/main/java/br/com/ciceroinfo/clinicajm/repository/PacienteRepository.java
@@ -0,0 +1,45 @@
+package br.com.ciceroinfo.clinicajm.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.ciceroinfo.clinicajm.domain.Paciente;
+
+@Repository
+public class PacienteRepository {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Transactional
+	public void salvaPaciente(Paciente paciente) {
+		entityManager.merge(paciente);
+	}
+
+	@Transactional
+	public void excluiPaciente(Integer id) {
+		Paciente paciente = entityManager.find(Paciente.class, id);
+		entityManager.remove(paciente);
+	}
+
+	@Transactional
+	public Paciente recuperaPaciente(Integer id) {
+		return entityManager.find(Paciente.class, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Paciente> listaPacientes() {
+
+		Query query = entityManager
+				.createQuery("SELECT p FROM Paciente p ORDER BY p.id");
+
+		return query.getResultList();
+	}
+
+}
